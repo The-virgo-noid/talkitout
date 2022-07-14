292f52875b4876fa85b826f7c1aad45339134da5
guido46ak <aniket.kasturi@gmail.com>
2022-07-14 17:37:21 +0530

    Updates

---
+++ precache-manifest.b3bf59f5c7f3dd989600f2888413e83d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1a0f715e5c53acbf284d92210e8ad0",
+    "url": "/talkitout/index.html"
+  },
+  {
+    "revision": "23a0390a096e3a35fae7",
+    "url": "/talkitout/static/css/main.1796080f.chunk.css"
+  },
+  {
+    "revision": "a790cd329d020d56179a",
+    "url": "/talkitout/static/js/2.6faf3461.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/talkitout/static/js/2.6faf3461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a0390a096e3a35fae7",
+    "url": "/talkitout/static/js/main.7f6e641f.chunk.js"
+  },
+  {
+    "revision": "da0a67fc0a416659bd96",
+    "url": "/talkitout/static/js/runtime-main.db6450f5.js"
+  }
+]);
